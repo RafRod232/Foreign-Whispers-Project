7d41a5c07c56e9ced12d3c2e74262e83450fa4d3
RafRod232 <89927101+RafRod232@users.noreply.github.com>
2023-11-19 00:01:45 -0500

    Updated translate.py to go through whisper_captions folder

--- translate.py
+++ translate.py
@@ -1,5 +1,6 @@
 import argostranslate.package
 import argostranslate.translate
+import os
 
 # from_code and to_code are language codes:
 def translate_text(text, from_code, to_code):
@@ -19,6 +20,17 @@ def translate_captions_file(file_path, from_code, to_code):
     captions_file.close()
     return translate_text(captions_text, from_code, to_code)
 
+def translate_captions_folder(folder_path, from_code, to_code, output_folder):
+    translated_texts = {}
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(folder_path, filename)
+            translated_text = translate_captions_file(file_path, from_code, to_code)
+            translated_texts[filename] = translated_text
+            output_file_path = os.path.join(output_folder, f"{filename.split('.')[0]}_french_translated.txt")
+            with open(output_file_path, "w") as output_file:
+                output_file.write(translated_text)
 
-translatedText = translate_captions_file("captions/Bill Gates_ The 2021 60 Minutes interview.srt", "en", "ko")
-print(translatedText)
+    return translated_texts
+os.makedirs("french_captions", exist_ok=True)
+translated_texts = translate_captions_folder("whisper_captions", "en", "fr", "french_captions")
